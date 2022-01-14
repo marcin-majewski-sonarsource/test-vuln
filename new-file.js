bac23ada099f8a5c80b3d01cf1978f08e2b38dbb
Marcin Majewski <marcin.majewski@sonarsource.com>
2022-01-14 12:45:03 +0100

    New commit

--- new-file.js
+++ new-file.js
@@ -1,1 +1,12 @@
 document.location = document.location.hash.slice(1);
+
+
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+
+const zip = new AdmZip("zip-slip.zip");
+const zipEntries = zip.getEntries();
+zipEntries.forEach(function (zipEntry) {
+  fs.createWriteStream(zipEntry.entryName); // Noncompliant
+});
+
